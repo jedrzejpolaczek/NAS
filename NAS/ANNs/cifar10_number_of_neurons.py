7f0b8347af9c090fb418f59fce6238082e4fa7df
Jedrzej Polaczek <jedrzej.polaczek@gmail.com>
2022-06-14 18:09:15 +0200

    feat: addjust network for working on CIFAR10.

--- NAS/ANNs/cifar10_number_of_neurons.py
+++ NAS/ANNs/cifar10_number_of_neurons.py
@@ -2,28 +2,20 @@ import tensorflow as tf
 import numpy as np
 
 
-def cifar_normalization(image, label):
-        """Normalizes images: `uint8` -> `float32`."""
-        return tf.cast(image, tf.float32) / 255., label
-
 def prepare_cifar10_data():
+    num_classes = 10
+
+    # Load data
     cifar10 = tf.keras.datasets.cifar10
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-    y_train = y_train.flatten()
-    y_test = y_test.flatten()
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-    # Normalization
-    # Reminder: input_shape = (32, 32, 3)
-
-    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 3)
-    x_train = x_train / 255.0
-    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 3)
-    x_test = x_test / 255.0
-
-    # Label Encoding
-    y_train = tf.one_hot(y_train.astype(np.int32), depth=10)
-    y_test = tf.one_hot(y_test.astype(np.int32), depth=10)
+    x_train = x_train.astype('float32')
+    x_train  /= 255
+    x_test = x_test.astype('float32')
+    x_test /= 255
 
     return x_train, y_train, x_test, y_test
 
@@ -33,20 +25,21 @@ def get_model(
     second_layer_number_of_neurons, 
     x_train, 
     y_train,
-    optimizer=tf.keras.optimizers.Adam(0.001),
+    optimizer='sgd',
     loss=tf.keras.losses.MeanSquaredError(),
-    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    metrics=['accuracy'],
     batch_size=32,
-    epochs=50
+    epochs=10
 ):
     model = tf.keras.models.Sequential(
         [
-            tf.keras.layers.Flatten(input_shape=(32, 32, 3)),
+            tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(first_layer_number_of_neurons, activation='relu'),
             tf.keras.layers.Dense(second_layer_number_of_neurons, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
         ]
     )
+
     model.compile(
         optimizer=optimizer,
         loss=loss,

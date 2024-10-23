f8bacd4d40a9873d08a319c3d3591f9b70ccfe7c
Jędrzej <jedrzej.polaczek@gmail.com>
2024-10-23 15:47:21 +0200

    feat: add unit tets for load config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,26 @@
+"""
+This module provides a function to load a JSON configuration file.
+
+Functions:
+    load_config(config_path): Loads a JSON configuration file from the specified path.
+"""
 import json
 
 
-def load_config(config_path):
+def load_config(
+    config_path: str
+) -> dict:
+    """
+    Loads a JSON configuration file from the specified path.
+
+    Args:
+        config_path (str):
+            The path to the JSON configuration file.
+
+    Returns:
+        dict:
+            The loaded JSON configuration as a dictionary.
+    """
+
     with open(config_path, "r", encoding="utf-8") as f:
         return json.load(f)
